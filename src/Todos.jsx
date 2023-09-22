c4318cfc423ab8d8ada3436cfa12c6c00dfcb0bb
Azure_Moon <jordanw0412@gmail.com>
2023-09-22 12:04:10 -0400

    added buttons and effects

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -1,14 +1,63 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 
 const Todos = () => {
-    const [value, setValue] = useState("")
-    const [todo, setTodo] = useState([])
-    return (
-        <div>
-            <input value={value} onChange={(e) => setValue(e.target.value)} />
-            {value}
-        </div>
-    )
+  const [value, setValue] = useState("");
+  const [todo, setTodo] = useState([]);
+
+// Displays options when list item is hovered
+  const handleHover = () => {
+    const button = document.getElementById('delete-button');
+    const completeButton = document.getElementById('complete-button');
+    completeButton.style.display = "inline"
+    button.style.display = "inline"
+};
+
+// Strikes through item if button is clicked
+const onCompleted = (index) => {
+    // setTodo((existingValue)=> {
+    //     const list = document.getElementById("list-item")
+    //    const listStyle = list.style.textDecoration = "line-through"
+       
+    // });
+
+  const list = document.getElementById("list-item")
+  list.style.textDecoration = "line-through"
 }
 
-export default Todos
+// Deletes input if button is clicked
+const onDelete = (index) => {
+  setTodo((existingValue)=> {
+      return existingValue.filter((_, currentIndex) => currentIndex !== index);
+  });
+};
+
+
+  
+// Inputs field into To Do list
+  const handleCLick = () => {
+    if (value === "") {
+      return "can't be empty";
+    }
+    setTodo([...todo, value]);
+  };
+
+  return (
+    <>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={handleCLick}>Add to List</button>
+      <div>
+        <ol>
+          {todo.map((todoItems, index) => (
+            <li id="list-item" key={index} onMouseOver={handleHover}>
+                {todoItems} 
+                <button id="delete-button" onClick={() => onDelete(index)}>Delete</button>
+                <button id="complete-button" onClick={() => onCompleted(index)}>Mark as Completed</button>
+            </li>
+          ))}
+        </ol>
+      </div>
+    </>
+  );
+};
+
+export default Todos;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 import './index.css'
-import Todos from './Todos.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Todos />
+    <App />
   </React.StrictMode>,
 )
